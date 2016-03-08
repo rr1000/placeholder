f9264b1f33cb85b23e4659a31c2cb8aa361d5afe
rr1000 <rrichrs@gmail.com>
2016-03-07 20:48:18 -0800

    Adding templates and some p10 content

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,8 @@
-// Removable if upgrading node to 0.12.*
-require('es6-promise').polyfill();
-
 var g = require('gulp');
 var gsass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
+var handlebars = require('gulp-compile-handlebars');
+var rename = require('gulp-rename');
 
 g.task('browserSync', function(){
     browserSync.init({
@@ -13,10 +12,30 @@ g.task('browserSync', function(){
     })
 });
 
-g.task('cssnano', function(){
-    return g.src('build/styles.css')
-        .pipe(cssnano())
-        .pipe(g.dest('build'))
+g.task('templates', function (){
+    var templateData = {
+        firstName: 'Kaanon'
+    },
+    options = {
+        ignorePartials: true,
+        partials : {
+            footer : '/build/partials/footer.handlebars',
+            header : '/build/partials/header.handlebars',
+            head : '/build/partials/head.handlebars'
+        },
+        batch : ['./build/partials'],
+        helpers : {
+            capitals : function(str){
+                return str.toUpperCase();
+            }
+        }
+    }
+    // Build p10 page
+    return g.src('build/p10/p10.handlebars')
+        .pipe(handlebars(templateData, options))
+        .pipe(rename('index.html'))
+        .pipe(g.dest('build/p10/'))
+        .pipe(browserSync.reload({ stream: true }))
 });
 
 g.task('sass', function(){
@@ -27,5 +46,7 @@ g.task('sass', function(){
 });
 
 g.task('watch', ['browserSync'], function(){
-    g.watch('build/styles.scss', ['sass']);
+    g.watch('build/*.scss', ['sass']);
+    g.watch('build/*.handlebars', ['templates']);
+    g.watch('build/*/*.handlebars', ['templates']);
 });
