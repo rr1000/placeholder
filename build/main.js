6c07ca39182dc82ed3e6519e7639755e99e43560
Ryan Rich <rrichrs@gmail.com>
2016-04-25 15:16:50 -0700

    Updating

---
+++ build/main.js
@@ -0,0 +1,12 @@
+$('document').ready(function(){
+
+    var menuBtn = $('.menu-btn');
+    var menu = $('nav');
+
+    menuBtn.click(function(){
+        menu.toggleClass('menu-retract');
+        $(this).toggleClass('menu-btn-extend');
+        $(this).html('Close');
+    });
+
+});
